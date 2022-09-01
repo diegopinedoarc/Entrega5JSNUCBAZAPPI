4ca73a2f38178dfc2128b13d1106a782417fae4c
Diego Pinedo <diegopinedo@outlook.com>
2022-09-01 20:39:57 -0300

    cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
+//Elementos HTML +---------
+const cartBTN = document.querySelector("#cartBTN");
+const cartSection = document.querySelector(".cart");
+//--------------------------
 //Seteo cart en localStorage
-let cart = json.parse(localStorage.getItem("cart")) || [];
+// let cart = json.parse(localStorage.getItem("cart")) || [];
 //Envio al localStorage de la lista del cart
-const saveLocalStorage = (cartList) => {
-  localStorage.setItem("cart", JSON.stringify(cartList));
-};
+// const saveLocalStorage = (cartList) => {
+//   localStorage.setItem("cart", JSON.stringify(cartList));
+// };
+
+cartBTN.addEventListener("click", (e) => {
+  e.preventDefault();
+  cartSection.classList.toggle("display");
+});
